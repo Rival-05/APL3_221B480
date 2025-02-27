ec02bfa2fdd82328a7ed8c884035a5cb30ec4031
Rajat Tripathi <rivalo3.chat@gmail.com>
2025-02-27 16:18:04 +0530

    Minor changes

--- LE4/Lazy/Singleton.java
+++ LE4/Lazy/Singleton.java
@@ -1,9 +1,11 @@
-//Eager Solution
+//Lazy Solution
 public class Singleton{
-    private static Singleton uniqueInstance = new Singleton();
-    private Singleton(){};
+    private static Singleton uniqueInstance;
+    private Singleton() {}
     public static Singleton getInstance(){
-        System.out.println("Inside get Instance fn");
+        if(uniqueInstance==null){
+            uniqueInstance= new Singleton();
+            System.out.println("if statement printed");}
         return uniqueInstance;
     }
 }
